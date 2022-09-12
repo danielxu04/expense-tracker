3e731781057da0fd1b826c3535f1ab579f99b06c
danielxu04 <danielxu004@gmail.com>
2022-09-11 20:30:12 -0400

    formatted expense.js to prepare for props

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function Expense(){
-    return <p>
-        Expense Item Tester.
-    </p>
+    return <>
+        <div>Date Placeholder</div>
+        <div>
+            <h2>Expense Name Placeholder</h2>
+            <div>Expense Price</div>
+        </div>
+    </>
 }
 
 export default Expense;
