8a8170741fcdfa3c0006f5d98e77c93cc521b580
danielxu04 <danielxu004@gmail.com>
2022-09-13 11:10:42 -0400

    introduced props into Expense function to improve reusability of component; created expenses.js file to store array of expenses; will map data to props soon - more efficient

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './Expense.css';
 
-function Expense(){
+
+function Expense(props){
     return <div className = "expense-item">
-        <div className = "expense-date">Date Placeholder</div>
+        <div className = "expense-date">{props.date.getFullYear()}-{props.date.getMonth()}-{props.date.getDate()}</div>
         <div className = "expense-description">
-            <h2>Expense Name Placeholder</h2>
-            <div className = "expense-price">$ Expense Price</div>
+            <h2>{props.name}</h2>
+            <div className = "expense-price">$ {props.price}</div>
         </div>
     </div>
 }
---
+++ src/expenses.js
@@ -0,0 +1,30 @@
+const expenses = [
+    {
+      id: 1,
+      name: 'Car',
+      cost: 41125.50,
+      date: new Date(2022, 9, 12),
+    },
+    {
+      id: 2,
+      name: 'Hamburger',
+      cost: 6.50,
+      date: new Date(2022, 9, 12),
+    },
+    {
+      id: 3,
+      name: 'Watch',
+      cost: 425.99,
+      date: new Date(2022, 9, 12),
+    },
+    {
+      id: 4,
+      name: 'Laptop',
+      cost: 2016.97,
+      date: new Date(2022, 9, 12),
+    },
+    
+  ]
+
+
+export default expenses;
