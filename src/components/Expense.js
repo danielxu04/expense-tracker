2fc423593f0b5071ee0f274f401e554b2fbda9cd
danielxu04 <danielxu004@gmail.com>
2022-09-12 23:24:23 -0400

    added css class selectors for other aspects of Expense component: date, description, and price

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -2,13 +2,13 @@ import React from "react";
 import './Expense.css';
 
 function Expense(){
-    return <>
-        <div>Date Placeholder</div>
-        <div>
+    return <div className = "expense-item">
+        <div className = "expense-date">Date Placeholder</div>
+        <div className = "expense-description">
             <h2>Expense Name Placeholder</h2>
-            <div>Expense Price</div>
+            <div className = "expense-price">$ Expense Price</div>
         </div>
-    </>
+    </div>
 }
 
 export default Expense;
