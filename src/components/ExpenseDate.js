bcaec3d689dd16d34f3d704437fcd6217a427ddb
danielxu04 <danielxu004@gmail.com>
2022-09-13 14:09:58 -0400

    created new css file for ExpenseDate component, along with a container class selector as well as a separate class selector for month, date, year

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import './ExpenseDate.css';
+
 function ExpenseDate(props){
     return (
-        <div className = "expense-date">
-            <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
-            <div>{props.date.getDate()}</div>
-            <div>{props.date.getFullYear()}</div>
+        <div className = "date-container">
+            <div className = "month">{props.date.toLocaleString('en-US', {month: 'long'})}</div>
+            <div className = "day">{props.date.getDate()}</div>
+            <div className = "year">{props.date.getFullYear()}</div>
         </div>
     );
 }
