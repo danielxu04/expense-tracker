a193003f8d63ab4ae24bf31657f113d55e7679aa
danielxu04 <danielxu004@gmail.com>
2022-09-20 09:37:35 -0400

    added css styling to the expense form items

--- src/components/NewExpense/CreateExpenseForm.js
+++ src/components/NewExpense/CreateExpenseForm.js
@@ -4,19 +4,19 @@ import "./CreateExpenseForm.css";
 
 function CreateExpenseForm(){
     return (<form>
-        <div>
-        <div>
-            <label>Expense</label>
-            <input type="text"></input>
-        </div>
-        <div>
-            <label>Date</label>
-            <input type="date" min="2020-01-01" max="2024-12-31"></input>
-        </div>
-        <div>
-            <label>Price</label>
-            <input type="number" min="0.01" step="0.01"></input>
-        </div>
+        <div className="form-items">
+            <div className="form-item">
+                <label>Expense</label>
+                <input type="text"></input>
+            </div>
+            <div className="form-item">
+                <label>Date</label>
+                <input type="date" min="2020-01-01" max="2024-12-31"></input>
+            </div>
+            <div className="form-item">
+                <label>Price</label>
+                <input type="number" min="0.01" step="0.01"></input>
+            </div>
         </div>
 
         <div>
