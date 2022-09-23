b4aea5e87a57bfa300846904fcf0c006796efb97
danielxu04 <danielxu004@gmail.com>
2022-09-23 14:59:46 -0400

    implemented react useState with enterItem object, refreshes state of expense, price, and date items

--- src/components/NewExpense/CreateExpenseForm.js
+++ src/components/NewExpense/CreateExpenseForm.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./CreateExpenseForm.css";
 
 function CreateExpenseForm(){
+
+    {/* item object that utilizes useState to modify properties */}
+    let [enterItem, setItem] = useState({
+        expense: "",
+        price: "",
+        date: ""
+    });
+
+    const expenseChanger = event => {
+        setItem( (prevState) => {
+            return {...prevState, expense: event.target.value};
+        });
+    };
+
+    const priceChanger = event => {
+        setItem( (prevState) => {
+            return {...prevState, price: event.target.value};
+        });
+    };
+
+    const dateChanger = event => {
+        setItem( (prevState) => {
+            return {...prevState, date: event.target.value}
+        });
+    };
+
+
     return (<form>
         <div className="form-items">
             <div className="form-item">
                 <label>Expense</label>
-                <input type="text"></input>
+                <input type="text" onChange={expenseChanger}></input>
             </div>
             <div className="form-item">
                 <label>Date</label>
-                <input type="date" min="2020-01-01" max="2024-12-31"></input>
+                <input type="date" min="2020-01-01" max="2024-12-31" onChange={dateChanger}></input>
             </div>
             <div className="form-item">
                 <label>Price</label>
-                <input type="number" min="0.01" step="0.01"></input>
+                <input type="number" min="0.01" step="0.01" onChange={priceChanger}></input>
             </div>
         </div>
 
