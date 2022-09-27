26b992d398fd2f5e4d5e3d576d36adabe1caafa4
danielxu04 <danielxu004@gmail.com>
2022-09-27 14:06:09 -0400

    implemented two way binding in expense form, synchronizing application data and global view

--- src/components/NewExpense/CreateExpenseForm.js
+++ src/components/NewExpense/CreateExpenseForm.js
@@ -4,11 +4,13 @@ import "./CreateExpenseForm.css";
 
 function CreateExpenseForm(){
 
-    {/* item object that utilizes useState to modify properties */}
+/*
+    STORE USERINPUT - using OBJECT
+
     let [enterItem, setItem] = useState({
-        expense: "",
-        price: "",
-        date: new Date("")
+        expense: '',
+        price: '',
+        date: new Date('')
     });
 
     const expenseChanger = event => {
@@ -30,24 +32,55 @@ function CreateExpenseForm(){
     };
 
     const submitter = (event) => {
-        event.preventDefault(); /* continue handling with javascript */
-
+        event.preventDefault(); 
         console.log(enterItem);
+    }
+*/
+    const [titleItem, setTitleItem] = useState('');
+    const [priceItem, setPriceItem] = useState('');
+    const [dateItem, setDateItem] = useState('');
+
+    const titleChanger = event => {
+        setTitleItem(event.target.value);
+    };
+    const priceChanger = event => {
+        setPriceItem(event.target.value);
+    };
+    const dateChanger = event => {
+        setDateItem(event.target.value);
+    }
+
+
+    const submitter = event => {
+        event.preventDefault();
+
+        const expenseItems = {
+            title: titleItem,
+            price: priceItem,
+            date: new Date(dateItem)
+        };
+
+        console.log(expenseItems);
+
+        /* Reset Items */
+        setTitleItem('');
+        setPriceItem('');
+        setDateItem('');
     }
 
     return (<form onSubmit={submitter}>
         <div className="form-items">
             <div className="form-item">
                 <label>Expense</label>
-                <input type="text" onChange={expenseChanger}></input>
+                <input type="text" value={titleItem} onChange={titleChanger}></input>
             </div>
             <div className="form-item">
                 <label>Date</label>
-                <input type="date" min="2020-01-01" max="2024-12-31" onChange={dateChanger}></input>
+                <input type="date" min="2020-01-01" max="2024-12-31" value={dateItem} onChange={dateChanger}></input>
             </div>
             <div className="form-item">
                 <label>Price</label>
-                <input type="number" min="0.01" step="0.01" onChange={priceChanger}></input>
+                <input type="number" min="0.01" step="0.01" value={priceItem} onChange={priceChanger}></input>
             </div>
         </div>
 
