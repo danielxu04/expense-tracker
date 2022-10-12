56a166465316f1fbc8c52edaa8bdce8456f07a04
danielxu04 <danielxu004@gmail.com>
2022-10-11 23:07:20 -0400

    added 'Add Expense' button, if triggered, pulls up ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./ExpenseForm.css";
 import CreateExpenseForm from "./CreateExpenseForm";
 
 function ExpenseForm (props){
+
     function uniqueId(){
         return Math.floor((Math.random() + 1) * 0x10000).toString(16).substring(1);
     }
@@ -13,13 +14,24 @@ function ExpenseForm (props){
             ...expenseData,
             key: uniqueId()
         };
-
         props.addExpense(data);
     };
 
+    const [displayForm, setDisplayForm] = useState(false);
+
+    const editForm = () => {
+        setDisplayForm(true);
+    }
+
+    let buttonOrForm = <button onClick={editForm}>Add Expense</button>;
+
+    if(displayForm){
+        buttonOrForm = <CreateExpenseForm newExpenseData={passNewExpenseData}/>;
+    }
+
     return (
-    <div className = "expense-form-container"> 
-        <CreateExpenseForm newExpenseData={passNewExpenseData}/>
+    <div className = "expense-form-container">
+        {buttonOrForm}
     </div >
     );
 }
