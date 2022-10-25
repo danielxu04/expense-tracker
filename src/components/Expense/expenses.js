831048fe9098e7d372e68049574fa1db9d95a7ad
danielxu04 <danielxu004@gmail.com>
2022-10-25 13:09:49 -0400

    added css styling to graph, modified placeholder expenses to demonstrate functionality of the web app

--- src/components/Expense/expenses.js
+++ src/components/Expense/expenses.js
@@ -1,29 +1,52 @@
 const expenses = [
     {
       key: 1,
-      name: 'Car',
-      price: 41125.50,
+      name: 'Bookshelf',
+      price: 150,
       date: new Date(2022, 9, 12),
     },
     {
       key: 2,
       name: 'Hamburger',
       price: 6.50,
-      date: new Date(2022, 9, 12),
+      date: new Date(2022, 3, 12),
     },
     {
       key: 3,
       name: 'Watch',
       price: 425.99,
-      date: new Date(2022, 9, 12),
+      date: new Date(2022, 6, 12),
     },
     {
       key: 4,
-      name: 'Laptop',
-      price: 2016.97,
-      date: new Date(2022, 9, 12),
+      name: 'Groceries',
+      price: 556.64,
+      date: new Date(2022, 4, 2),
     },
-    
+    {
+      key: 5,
+      name: 'Soccer Ball',
+      price: 80.99,
+      date: new Date(2021, 11, 24),
+    },
+    {
+      key: 6,
+      name: 'Basketball',
+      price: 94.34,
+      date: new Date(2020, 2, 17),
+    },
+    {
+      key: 7,
+      name: 'Electric Toothbrush',
+      price: 40.99,
+      date: new Date(2021, 9, 12),
+    },
+    {
+      key: 8,
+      name: 'Cologne',
+      price: 180.12,
+      date: new Date(2021, 6, 14),
+    }
   ]
 
 
